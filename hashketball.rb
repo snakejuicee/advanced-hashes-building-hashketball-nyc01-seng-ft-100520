70fc130267c9f0b7b359fb93ffece3fb7fd643fc
Patricia Arnedo <paaarnedo@gmail.com>
2020-10-02 10:01:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,17 @@
 # Write your code here!
+def game_hash()
+  
+  hash = {
+    :home = {
+      :team_name = ""
+      :colors = []
+      :players
+    }
+    :away = {
+      :team_name = ""
+      :colors = []
+      :players
+    }
+  }
+  
+end
