b787b0abb4b61540b091f2ae7cf6c651e85d7737
Patricia Arnedo <paaarnedo@gmail.com>
2020-10-02 10:36:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -68,7 +68,7 @@ def game_hash()
       colors: ["Turquoise", "Purple"],
       players: [
         "Jeff Adrien" => {
-          
+          player_name: "Jeff Adrien",
           number: 4,
           shoe: 18,
           points: 10,
@@ -79,7 +79,7 @@ def game_hash()
           slam_dunks: 2
         },
         "Bismack Biyombo" => {
-          
+          player_name: "Bismack Biyombo",
           number: 0,
           shoe: 16,
           points: 12,
@@ -90,7 +90,7 @@ def game_hash()
           slam_dunks: 10
         },
         "DeSagna Diop" => {
-         
+          player_name: "DeSagna Diop",
           number: 2,
           shoe: 14,
           points: 24,
@@ -101,7 +101,7 @@ def game_hash()
           slam_dunks: 5
         },
         "Ben Gordon" => {
-          
+          player_name: "Ben Gordon",
           number: 8,
           shoe: 15,
           points: 33,
@@ -112,7 +112,7 @@ def game_hash()
           slam_dunks: 0
         },
         "Kemba Walker" => {
-          
+          player_name: "Kemba Walker",
           number: 33,
           shoe: 15,
           points: 6,
