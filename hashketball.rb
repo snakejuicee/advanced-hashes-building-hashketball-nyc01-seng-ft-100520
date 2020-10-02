5347232850fc5635f9fec0cc56e3d0393ba3b570
Patricia Arnedo <paaarnedo@gmail.com>
2020-10-02 10:39:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,7 +17,7 @@ def game_hash()
           blocks: 1,
           slam_dunks: 1
         },
-        "Reggie Evans" => {
+        {
           player_name: "Reggie Evans",
           number: 30,
           shoe: 14,
@@ -28,7 +28,7 @@ def game_hash()
           blocks: 12,
           slam_dunks: 7
         },
-        "Brook Lopez" => {
+        {
           player_name: "Brook Lopez",
           number: 11,
           shoe: 17,
@@ -39,7 +39,7 @@ def game_hash()
           blocks: 1,
           slam_dunks: 15
         },
-        "Mason Plumlee" => {
+        {
           player_name: "Mason Plumlee",
           number: 1,
           shoe: 19,
@@ -50,7 +50,7 @@ def game_hash()
           blocks: 8,
           slam_dunks: 5
         },
-        "Jason Terry" => {
+        {
           player_name: "Jason Terry",
           number: 31,
           shoe: 15,
@@ -67,7 +67,7 @@ def game_hash()
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
       players: [
-        "Jeff Adrien" => {
+        {
           player_name: "Jeff Adrien",
           number: 4,
           shoe: 18,
@@ -78,7 +78,7 @@ def game_hash()
           blocks: 7,
           slam_dunks: 2
         },
-        "Bismack Biyombo" => {
+        {
           player_name: "Bismack Biyombo",
           number: 0,
           shoe: 16,
@@ -89,7 +89,7 @@ def game_hash()
           blocks: 15,
           slam_dunks: 10
         },
-        "DeSagna Diop" => {
+        {
           player_name: "DeSagna Diop",
           number: 2,
           shoe: 14,
@@ -100,7 +100,7 @@ def game_hash()
           blocks: 5,
           slam_dunks: 5
         },
-        "Ben Gordon" => {
+        {
           player_name: "Ben Gordon",
           number: 8,
           shoe: 15,
@@ -111,7 +111,7 @@ def game_hash()
           blocks: 1,
           slam_dunks: 0
         },
-        "Kemba Walker" => {
+        {
           player_name: "Kemba Walker",
           number: 33,
           shoe: 15,
